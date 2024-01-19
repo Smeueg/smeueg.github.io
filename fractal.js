21cbdc7667bbdb1d7927d7393abbc3a8a24eb8bf
Smeueg <Smeueg@gmail.com>
2024-01-19 11:57:53 +0700

    Added a slider

--- fractal.js
+++ fractal.js
@@ -1,24 +1,34 @@
 let canvas = document.getElementById("canvas");
 let canvasContext = canvas.getContext("2d");
-canvasContext.strokeStyle = "#fe8019";
+
 let max_angle = Math.PI;
 let angle = Math.PI / 6;
 
-function update(event) {
+canvas.height = Math.min(window.screen.height, window.screen.width) * 2 / 3;
+canvas.width = canvas.height;
+canvasContext.strokeStyle = "#fe8019";
+
+function drawFirstBranch() {
 		canvasContext.clearRect(0, 0, canvas.width, canvas.height);
 		canvasContext.beginPath();
-		angle = event.clientX / canvas.width * max_angle;
-		branch(canvas.width / 2, canvas.height, canvas.height / 5, 0, 8);
+		drawBranch(canvas.width / 2, canvas.height, canvas.height / 3, 0, 8);
+		canvasContext.stroke()
 		canvasContext.closePath();
 }
 
-function branch(x, y, len, deg, step) {
+function drawBranch(x, y, len, deg, step) {
 		if (step === 0) { return; }
 		let x_end = x - Math.cos(deg + Math.PI / 2) * len;
 		let y_end = y - Math.sin(deg + Math.PI / 2) * len;
 		canvasContext.moveTo(x, y);
 		canvasContext.lineTo(x_end, y_end);
-		branch(x_end, y_end, len * 0.67, deg - angle, step - 1)
-		branch(x_end, y_end, len * 0.67, deg + angle, step - 1)
-		canvasContext.stroke();
+		drawBranch(x_end, y_end, len * 0.67, deg - angle, step - 1)
+		drawBranch(x_end, y_end, len * 0.67, deg + angle, step - 1)
 }
+
+document.getElementById("degSlider").oninput = function() {
+		angle = this.value / 100 * Math.PI;
+		drawFirstBranch();
+}
+
+drawFirstBranch()
